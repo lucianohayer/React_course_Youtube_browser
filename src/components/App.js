6740b9e1db4f187c1ce4b8455f7dace7a37f1765
Luciano Hayer <lucianohayer@gmail.com>
2021-08-16 01:31:21 +0200

    Youtube API Request

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    try {
+      const response = await youtube.get('/search', {
+        params: { q: term }
+      });
+      this.setState({ videos: response.data && response.data.items ? response.data.items : [] });
+
+    } catch (err) {
+      console.error('Error fetching yuotube videos ...')
+      this.setState({ videos: [] });
+    }
+  };
   
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={ this.onTermSubmit } />
+        I have {this.state.videos.length} videos ...
       </div>
     );
-  }
+  };
 }
 
 export default App;
