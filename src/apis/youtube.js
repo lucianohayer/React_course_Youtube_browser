e23574f3a3eb7ff878e5866d9bff3312e54f8d94
Luciano Hayer <lucianohayer@gmail.com>
2021-08-16 01:11:06 +0200

    Ignore api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
-
-const KEY = '';
+import { YOUTUBE_API_KEY } from '../properties/youtube_api_key';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: KEY,
+    key: YOUTUBE_API_KEY,
     type: 'video'
   }
 });
