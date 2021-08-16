5dda3b6e230b05271c75d03d1b79c0dd65b5f405
Luciano Hayer <lucianohayer@gmail.com>
2021-08-16 23:04:55 +0200

    Video detail implementation

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,51 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
 
   onTermSubmit = async term => {
     try {
       const response = await youtube.get('/search', {
         params: { q: term }
       });
-      this.setState({ videos: response.data && response.data.items ? response.data.items : [] });
+      this.setState({ 
+        videos: response.data && response.data.items ? response.data.items : [],
+        selectedVideo: response.data && response.data.items && response.data.items.length ? response.data.items[0] : null
+      });
 
     } catch (err) {
       console.error('Error fetching yuotube videos ...')
       this.setState({ videos: [] });
     }
   };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  }
   
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={ this.onTermSubmit } />
-        <VideoList videos={ this.state.videos }/>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={ this.state.selectedVideo } />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={ this.onVideoSelect } videos={ this.state.videos } />
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
